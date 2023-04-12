97385dbbcd13153a86acd27e843519bb8e881ea4
CmEinsig <cmneiderer@gmail.com>
2023-04-12 18:57:17 -0400

    worked on code

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
-.// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What do you need to run your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+      },
+      {
+        type: 'list',
+        message: 'What liscence are you using?',
+        name: 'liscence',
+        choices: ['MIT', 'Creative Commons', 'None'],
+      }, 
+      {
+        type: 'input',
+        name: 'contribute',
+        message: 'Ways to contribute to your project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What kind of tests for your project?',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Email to contact you?',
+      },
+    ];
+    console.log(questions)
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  inquirer.prompt(questions).then((answers) => {
+    generateREADME(answers);
+  });
 
-// TODO: Create a function to initialize app
-function init() {}
+  function generateREADME(readme)
+${title}
 
-// Function call to initialize app
-init();
+Description
+    ${description}
+Table of Contents
+
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Contribute](#contribute)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    Installation
+    ${installation}
+
+Usage
+    ${usage}
+    
+Credits
+    ${credits}
+
+Liscense
+${liscense}
+    
+How to Contribute
+
+    ${contribute}
+
+Tests
+    ${tests}
+
+Questions
+    You can email me any questions at ${questions}
+
+    }
+;
+
+fs.writeFile('README.me', readme, (err) => {
+    if (err) throw err;
+    console.log('README generated!');
+  });
